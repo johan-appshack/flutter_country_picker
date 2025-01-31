a1d20362309b6f32aacab24f1e41af6ff82418e4
jönne <johan.joensson@appshack.se>
2025-01-31 10:58:17 +0100

    Sort country list by locale

--- lib/src/country_list_view.dart
+++ lib/src/country_list_view.dart
@@ -61,9 +61,9 @@ class CountryListView extends StatefulWidget {
     this.showSearch = true,
     this.customFlagBuilder,
   })  : assert(
-  exclude == null || countryFilter == null,
-  'Cannot provide both exclude and countryFilter',
-  ),
+          exclude == null || countryFilter == null,
+          'Cannot provide both exclude and countryFilter',
+        ),
         super(key: key);
 
   @override
@@ -101,13 +101,13 @@ class _CountryListViewState extends State<CountryListView> {
 
     if (widget.exclude != null) {
       _countryList.removeWhere(
-            (element) => widget.exclude!.contains(element.countryCode),
+        (element) => widget.exclude!.contains(element.countryCode),
       );
     }
 
     if (widget.countryFilter != null) {
       _countryList.removeWhere(
-            (element) => !widget.countryFilter!.contains(element.countryCode),
+        (element) => !widget.countryFilter!.contains(element.countryCode),
       );
     }
 
@@ -126,6 +126,20 @@ class _CountryListViewState extends State<CountryListView> {
         CountryLocalizations.of(context)?.countryName(countryCode: 'search') ??
             'Search';
 
+    _filteredList.sort(
+      (a, b) {
+        final aL = CountryLocalizations.of(context)
+                ?.countryName(countryCode: a.countryCode)
+                ?.replaceAll(RegExp(r"\s+"), " ") ??
+            a.name;
+        final bL = CountryLocalizations.of(context)
+                ?.countryName(countryCode: b.countryCode)
+                ?.replaceAll(RegExp(r"\s+"), " ") ??
+            b.name;
+        return aL.compareTo(bL);
+      },
+    );
+
     return Column(
       children: <Widget>[
         const SizedBox(height: 12),
@@ -136,7 +150,7 @@ class _CountryListViewState extends State<CountryListView> {
               autofocus: _searchAutofocus,
               controller: _searchController,
               style:
-              widget.countryListTheme?.searchTextStyle ?? _defaultTextStyle,
+                  widget.countryListTheme?.searchTextStyle ?? _defaultTextStyle,
               decoration: widget.countryListTheme?.inputDecoration ??
                   InputDecoration(
                     labelText: searchLabel,
@@ -164,6 +178,7 @@ class _CountryListViewState extends State<CountryListView> {
                 ),
               ],
               ..._filteredList
+                  .where((country) => !country.iswWorldWide)
                   .map<Widget>((country) => _listRow(country))
                   .toList(),
             ],
@@ -219,8 +234,8 @@ class _CountryListViewState extends State<CountryListView> {
               Expanded(
                 child: Text(
                   CountryLocalizations.of(context)
-                      ?.countryName(countryCode: country.countryCode)
-                      ?.replaceAll(RegExp(r"\s+"), " ") ??
+                          ?.countryName(countryCode: country.countryCode)
+                          ?.replaceAll(RegExp(r"\s+"), " ") ??
                       country.name,
                   style: _textStyle,
                 ),
@@ -242,19 +257,19 @@ class _CountryListViewState extends State<CountryListView> {
   }
 
   Widget _emojiText(Country country) => Text(
-    country.iswWorldWide
-        ? '\uD83C\uDF0D'
-        : Utils.countryCodeToEmoji(country.countryCode),
-    style: TextStyle(
-      fontSize: widget.countryListTheme?.flagSize ?? 25,
-      fontFamilyFallback: widget.countryListTheme?.emojiFontFamilyFallback,
-    ),
-  );
+        country.iswWorldWide
+            ? '\uD83C\uDF0D'
+            : Utils.countryCodeToEmoji(country.countryCode),
+        style: TextStyle(
+          fontSize: widget.countryListTheme?.flagSize ?? 25,
+          fontFamilyFallback: widget.countryListTheme?.emojiFontFamilyFallback,
+        ),
+      );
 
   void _filterSearchResults(String query) {
     List<Country> _searchResult = <Country>[];
     final CountryLocalizations? localizations =
-    CountryLocalizations.of(context);
+        CountryLocalizations.of(context);
 
     if (query.isEmpty) {
       _searchResult.addAll(_countryList);
